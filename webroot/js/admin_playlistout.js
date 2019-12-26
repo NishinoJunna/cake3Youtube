ea7aae96b614c1b4ea564c90f7972622a5d1921a
PatientYuki <naoe.patient.yuki@no1s.biz>
2019-12-26 14:24:01 +0900

    pat

--- webroot/js/admin_playlistout.js
+++ webroot/js/admin_playlistout.js
@@ -1,5 +1,5 @@
-var apiKey = 'AIzaSyAyvuTLQlXGhiqc5i85uuw9ewsMRXJkHKQ';
-//var apiKey = 'AIzaSyDhDHWAYFM1P-Y39DnrXrs3ltU0Qg1YLBU';
+//var apiKey = 'AIzaSyAyvuTLQlXGhiqc5i85uuw9ewsMRXJkHKQ';
+var apiKey = 'AIzaSyDhDHWAYFM1P-Y39DnrXrs3ltU0Qg1YLBU';
 var url_string = window.location.href;
 var url = new URL(url_string);
 var youtube_id = url.searchParams.get("youtube_id");
